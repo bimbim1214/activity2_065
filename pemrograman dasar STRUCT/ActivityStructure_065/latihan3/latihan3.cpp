3e54e114ee94665c9b3d76c4b1943357d7187f1f
bimbim1214 <bimoadityapangestu@gmail.com>
2023-04-05 11:37:30 +0700

    membuat int main

--- pemrograman dasar STRUCT/ActivityStructure_065/latihan3/latihan3.cpp
+++ pemrograman dasar STRUCT/ActivityStructure_065/latihan3/latihan3.cpp
@@ -15,3 +15,29 @@ struct Mahasiswa
 	int umur;
 };
 
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; i < 3; i++) {
+		cout << "masukan NIM : ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "masukan umur : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << "masukan nama : ";
+		cin.getline(mhs[i].nama, 25);
+		cout << "alamat : ";
+		cout << "\n\tmasukan desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\n\tmasukan kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << "\n\nNIM : " << mhs[i].nim;
+		cout << "\numur : " << mhs[i].umur;
+		cout << "\nnama : " << mhs[i].nama;
+		cout << "\nalamat : ";
+		cout << "\n\tdesa : " << mhs[i].alamat.desa;
+		cout << "\n\tkota : " << mhs[i].alamat.kota;
+	}
+}
